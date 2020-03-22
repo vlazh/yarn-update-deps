b1f1cbff84a05a6a87ff8bbc24aa0ad3a4f2717f
Vladimir <vzh.inbox@gmail.com>
2020-03-22 15:46:05 +0600

    Add yarn-update-deps vscode extension

---
+++ README.md
@@ -0,0 +1,3 @@
+# yarn-update-deps README
+
+Run `yarn upgrade-interactive --latest`
---
+++ src/extension.ts
@@ -0,0 +1,73 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+// eslint-disable-next-line import/no-unresolved
+import * as vscode from 'vscode';
+
+function showError(message: string): void {
+  vscode.window.showErrorMessage(`Yarn update-deps: ${message}`);
+}
+
+function runCommand(workspace: vscode.WorkspaceFolder): void {
+  const task: vscode.Task = new vscode.Task(
+    { type: 'yarn' },
+    workspace,
+    'update-deps',
+    'yarn',
+    new vscode.ShellExecution(
+      // https://misc.flogisoft.com/bash/tip_colors_and_formatting
+      // eslint-disable-next-line no-template-curly-in-string
+      "echo -e '\\e[1;97;100m Run update-deps for \\e[102m ${workspaceFolderBasename} \\e[0m\n' && yarn upgrade-interactive --latest"
+    )
+  );
+  task.isBackground = false;
+  task.presentationOptions.echo = false;
+  task.presentationOptions.focus = true;
+  task.runOptions.reevaluateOnRerun = true;
+
+  vscode.tasks.executeTask(task).then(undefined, (err) => {
+    console.error(err);
+    showError(err.toString());
+  });
+}
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext): void {
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  const disposable = vscode.commands.registerCommand('extension.update-deps', () => {
+    // The code you place here will be executed every time your command is executed
+
+    const workspaceFolders = vscode.workspace.workspaceFolders ?? [];
+
+    // Must have at least one workspace folder
+    if (workspaceFolders.length === 0) {
+      showError('You must have a workspace opened.');
+      return;
+    }
+
+    // If in a multifolder workspace, prompt user to select which one to traverse.
+    if (workspaceFolders.length > 1) {
+      vscode.window
+        .showQuickPick(
+          workspaceFolders.map((folder) => ({ label: folder.name, folder })),
+          { placeHolder: 'Select workspace folder' }
+        )
+        .then(
+          (selected) => selected && runCommand(selected.folder),
+          (err) => showError(err.toString())
+        );
+    } else {
+      // Otherwise, use the first one
+      const folder = workspaceFolders[0];
+      runCommand(folder);
+    }
+  });
+
+  context.subscriptions.push(disposable);
+}
+
+// this method is called when your extension is deactivated
+// eslint-disable-next-line @typescript-eslint/no-empty-function
+export function deactivate(): void {}
